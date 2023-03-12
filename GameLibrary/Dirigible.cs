8f58f9c0ed7757b7d533884419150ddaa013c047
Calmbl4 <boss.buyakevich@mail.ru>
2023-03-12 18:52:08 +0300

    Добавлены классы Dirigible и GameProcessing

---
+++ GameLibrary/Dirigible.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibrary
+{
+
+    public class Dirigible
+    {
+        public int Health { get; set; } = 10;
+        public int Armor { get; set; } = 10;
+
+        public bool IsAlive(int health,int armor)
+        {
+            if (health <= 0 && armor <= 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
